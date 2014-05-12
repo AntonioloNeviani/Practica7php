21f61dfe5a14afeca73e80de673c165f947e6a4f
Jorge Antonio <jorgeant2000@hotmail.com>
2014-05-11 22:40:27 -0500

    Update Session.php

--- Session.php
+++ Session.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8"/>
@@ -9,15 +10,20 @@
 ?>
 <br/>
 <?php
-$nombreAbmin = "abmin";
+$nombreAdmin = "abmin";
 $passwordAdmin = "123";
-    if($nombreAbmin == $_POST['nombre'] || $passwordAbmin == $_POST['password']){
+    if($nombreAdmin == $_POST['nombre'] && $passwordAdmin == $_POST['password']){
         $_SESSION['nombre'] = $_POST['nombre'];
         $_SESSION['password'] = $_POST['password'];
         echo "<p><a href='Cerrar.php'>Cerrar Sesion</a></p>";
         echo "Bienvenido! ".$_POST['nombre'];
     }else{
-        echo "Acceso Restringido intente de nuevo";
+        if(isset($_SESSION['nombre'])){
+            echo "<p><a href='Cerrar.php'>Cerrar Sesion</a></p>";
+            echo "Bienvenido! ".$_POST['nombre'];
+        }else{
+            echo "Acceso Restringido intente de nuevo";
+        }
     }
 ?>
 <br /><a href="Index.php">Ir a Inicio</a>
